96cf0e677f62beccc2d64b008eabacbc8592100d
bra2aesam <bra2aesam1994@gmail.com>
2023-03-12 16:04:23 +0300

    bb

---
+++ main.js
@@ -0,0 +1,6 @@
+function changeBG() {
+    document.getElementById('btn-Js').style.backgroundColor = "purple";
+}
+function returnBG() {
+    document.getElementById('btn-Js').style.backgroundColor = "#f53838";
+}
